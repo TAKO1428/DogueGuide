262d9751a3415e3f1e2a2f20b2992543da6e75df
TAKO1428 <124121402+TAKO1428@users.noreply.github.com>
2024-01-16 12:57:02 -0600

    Add files via upload

---
+++ Doge_Guide-main/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+
+from chatbot import ChatBot
+
+cb = ChatBot()
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('chat.html')
+
+
+@app.route("/get", methods=["GET", "POST"])
+def chat():
+    msg = request.form["msg"]
+    input = msg
+    return cb.get_response(input)
+
+
+if __name__ == '__main__':
+    app.run()
